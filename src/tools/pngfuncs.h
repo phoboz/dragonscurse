ef6e922a8b6bed8a85e45a0d6fcab2be8afa8d73
phoboz <leputa6@hotmail.com>
2013-02-01 06:37:34 +0100

    Fixed include directives

--- src/tools/pngfuncs.h
+++ src/tools/pngfuncs.h
@@ -21,11 +21,10 @@
    you can find other examples on http://marsnomercy.org
 */
 
-#include <png.h>
-#include <SDL/SDL.h>
+#include "png.h"
+#include "SDL.h"
 
 
-//static int png_colortype_from_surface(SDL_Surface *surface);
 void png_user_warn(png_structp ctx, png_const_charp str);
 void png_user_error(png_structp ctx, png_const_charp str);
 int png_save_surface(char *filename, SDL_Surface *surf);
