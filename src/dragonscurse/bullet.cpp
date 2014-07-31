ad68e5efb42f524699eee43914e3dbb8e8f2ca63
baijkal <baijkal@hotmail.com>
2014-07-31 15:47:01 +0200

    Generalized Bullet class

--- src/dragonscurse/bullet.cpp
+++ src/dragonscurse/bullet.cpp
@@ -1,63 +1,40 @@
 #include <math.h>
 #include "bullet.h"
 
-void Bullet::reload()
+void Bullet::set_dir(Direction dir)
 {
-    m_loaded = false;
-    m_distance = 0;
-    m_reload_timer.reset();
-}
-
-bool Bullet::fire(int x, int y, int dx, int dy)
-{
-    bool result = false;
-
-    if (!m_moving && m_loaded) {
-        m_x = x;
-        m_y = y;
-
-        if (dx > 0) {
-            m_dir = Right;
-            m_dx = dx;
-            m_frame = get_attribute("right_move_start");
-        }
-        else if (dx < 0) {
-            m_dir = Left;
-            m_dx = -dx;
-            m_frame = get_attribute("left_move_start");
-        }
-
-        if (dy < 0) {
-            m_vertical_dir = VerticalUp;
-            m_dy = -dy;
-        }
-        else if (dy > 0) {
-            m_vertical_dir = VerticalDown;
-            m_dy = dy;
-        }
-
-        m_moving = true;
-        reload();
-        result = true;
+    if (dir == Right) {
+        m_frame = get_attribute("right_move_start");
+    }
+    else if (dir == Left) {
+        m_frame = get_attribute("left_move_start");
     }
 
-    return result;
+    Body::set_dir(dir);
+}
+
+void Bullet::reload(bool active)
+{
+    m_ready = false;
+    m_distance = 0;
+    m_reload_timer.reset();
+    m_active = active;
 }
 
 bool Bullet::hit_object(Object *object)
 {
     bool result = false;
 
-    if (m_moving) {
+    if (m_active) {
         if (object->check_shielded_collision(this)) {
             // Remove bullet if it hits a shielded area
-            m_moving = false;
+            reload(false);
         }
         else if (object->check_weak_collision(this)) {
 
             // Remove bullet if it only is supposed to hit one target
             if (m_hit_one) {
-                m_moving = false;
+                reload(false);
             }
             result = true;
         }
@@ -69,46 +46,21 @@ bool Bullet::hit_object(Object *object)
 void Bullet::move(Map *map)
 {
     if (m_reload_timer.expired(get_attribute("reload_time"))) {
-        m_loaded = true;
+        m_ready = true;
     }
 
-    if (m_moving) {
-        if (m_follow_terrain) {
-            check_ahead(map);
-        }
-        if (m_vertical_dir == VerticalDown) {
-            if (m_follow_terrain) {
-                check_below(map);
-            }
-        }
-        else if (m_vertical_dir == VerticalUp) {
-            if (m_follow_terrain) {
-                check_above(map);
-            }
-        }
-        if (m_dx || m_dy) {
-            if (m_distance < get_attribute("distance")) {
-                if (m_dir == Right) {
-                    m_x += m_dx;
-                }
-                else if (m_dir == Left) {
-                    m_x -= m_dx;
-                }
-                if (m_vertical_dir == VerticalDown) {
-                    m_y += m_dy;
-                }
-                else if (m_vertical_dir == VerticalUp) {
-                    m_y -= m_dy;
-                }
+    if (m_active) {
+        if (m_distance < get_attribute("distance")) {
+            Body::move(map);
+            if (get_moving()) {
                 m_distance += sqrt(m_dx * m_dx + m_dy * m_dy);
             }
             else {
-                m_moving = false;
-                reload();
+                reload(false);
             }
         }
         else {
-            m_moving = false;
+            reload(false);
         }
     }
 }
